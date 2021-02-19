f7595c739fab4bfb192309533cb14c4aea95ad39
Ferhan609 <76482698+Ferhan609@users.noreply.github.com>
2021-02-19 14:38:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var block1,block2,block3,block4,block5,block6;
+var stage1;
+var polygon;
+ function setup(){
+    var canvas = createCanvas(400,400);
+    background=("grey");
+
+    ground = new Ground(200,300,200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    block1 = new block(100,300,20,30);
+    block2 = new block(150,325,20,30);
+    block3 = new block(200,350,20,30);
+    block4 = new block(125,225,20,30);
+    block5 = new block(175,275,20,30);
+    block6=  new block(150,100,20,30)
+
+ }
+function draw(){
+
+
+
+
+
+
+
+
+}
